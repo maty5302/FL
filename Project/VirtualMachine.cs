deca1bdf369a3385d4ed68bb0ce846cd51a7a66d
Maty5302 <matej.prudil@outlook.cz>
2024-04-22 10:07:16 +0200

    VirtualMachine processes more instructions (PLC_t2.in and PLC_t3.in successed)
    Took 35 minutes

--- Project/VirtualMachine.cs
+++ Project/VirtualMachine.cs
@@ -18,8 +18,9 @@ public class VirtualMachine
     
     public void Run()
     {
-        foreach (var instruction in this.codeInstructions)
+        for (int i = 0; i < this.codeInstructions.Count; i++)
         {
+            var instruction = this.codeInstructions[i];
             if (instruction[0].StartsWith("push"))
             {
                 if (instruction[1] == "I")
@@ -29,8 +30,8 @@ public class VirtualMachine
                 else if (instruction[1] == "S")
                 {
                     string final = "";
-                    for (int i = 2; i < instruction.Length; i++){
-                        final += instruction[i].Replace("\"","") + " ";
+                    for (int j = 2; j < instruction.Length; j++){
+                        final += instruction[j].Replace("\"","") + " ";
                     }
                     stack.Push(final.Trim());
                 }
@@ -40,17 +41,17 @@ public class VirtualMachine
             else if (instruction[0].StartsWith("print"))
             {
                 string[] words = new string[int.Parse(instruction[1])];
-                for(int i=0; i<int.Parse(instruction[1]); i++)
+                for(int j=0; j<int.Parse(instruction[1]); j++)
                 {
                     var value = stack.Pop();
                     if(value is int)
-                        words[i] = (int)value + " ";
+                        words[j] = (int)value + " ";
                     else if(value is float)
-                        words[i]= ((float)value).ToString() + " ";
+                        words[j]= ((float)value).ToString() + " ";
                     else if(value is bool)
-                        words[i]= (bool)value + " ";
+                        words[j]= (bool)value + " ";
                     else 
-                        words[i]= (string)value + " ";
+                        words[j]= (string)value + " ";
                 }
                 Array.Reverse(words);
                 string final = string.Join(" ", words);
@@ -166,6 +167,71 @@ public class VirtualMachine
             else if (instruction[0].StartsWith("pop"))
             {
                 stack.Pop();
+            }
+            else if (instruction[0].StartsWith("eq"))
+            {
+                var right = stack.Pop();
+                var left = stack.Pop();
+                if (left is int && right is int)
+                    stack.Push((int)left == (int)right);
+                else if (left is float && right is float)
+                    stack.Push((float)left == (float)right);
+                else if (left is bool && right is bool)
+                    stack.Push((bool)left == (bool)right);
+                else if (left is string && right is string)
+                    stack.Push((string)left == (string)right);
+            }
+            else if (instruction[0].StartsWith("gt"))
+            {
+                var right = stack.Pop();
+                var left = stack.Pop();
+                if (left is int && right is int)
+                    stack.Push((int)left > (int)right);
+                else if (left is float && right is float)
+                    stack.Push((float)left > (float)right);
+            }
+            else if(instruction[0].StartsWith("lt"))
+            {
+                var right = stack.Pop();
+                var left = stack.Pop();
+                if (left is int && right is int)
+                    stack.Push((int)left < (int)right);
+                else if (left is float && right is float)
+                    stack.Push((float)left < (float)right);
+            }
+            else if (instruction[0].StartsWith("jmp"))
+            {
+                var label = instruction[1];
+                for (int j = 0; j < this.codeInstructions.Count; j++)
+                {
+                    if (this.codeInstructions[j][0] == "label" && this.codeInstructions[j][1] == label)
+                    {
+                        j--;
+                        i = j;
+                        break;
+                    }
+                }
+            }
+            else if (instruction[0].StartsWith("fjmp"))
+            {
+                var label = instruction[1];
+                var condition = stack.Pop();
+                if (!(bool)condition)
+                {
+                    for (int j = 0; j < this.codeInstructions.Count; j++)
+                    {
+                        if (this.codeInstructions[j][0] == "label" && this.codeInstructions[j][1] == label)
+                        {
+                            j--;
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (instruction[0].StartsWith("label"))
+            {
+                // do nothing
             }
             else
             {
